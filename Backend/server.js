49cb434f707a776dba653e40cc31ec46bdcead77
Pranav P. Kadam <pranavkadam320@gmail.com>
2024-12-31 20:20:34 +0530

    Added Server File

--- Backend/server.js
+++ Backend/server.js
@@ -81,5 +81,5 @@ app.get("/api/sent-emails", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is ready to run on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
